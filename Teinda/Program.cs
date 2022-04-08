3e436defd59e6a38e6aeac19581998202d3ca491
Crodas451 <101899953+Crodas451@users.noreply.github.com>
2022-04-08 12:08:44 -0600

    Update Program.cs
    MEnu

--- Teinda/Program.cs
+++ Teinda/Program.cs
@@ -12,20 +12,46 @@ namespace Teinda
         {
             C_CLiente cLiente = new C_CLiente();
 
-            int id;
+            int id, op;
             string name,tele,ape,dire;
-            Console.Write("INGRESE N.CARNE.");
-            id = Convert.ToInt32(Console.ReadLine());
-            Console.Write("INGRESE NOMBRE..");
-            name = Console.ReadLine();
-            Console.Write("INGRESE APELLIDO");
-            ape = Console.ReadLine(); 
-            Console.Write("INGRESE TELEFONO");
-            tele = Console.ReadLine();
-            Console.Write("DIRECCION.......");
-            dire = Console.ReadLine();
+            do
+            {
+                Console.Write("1- AGG CIENTE");
+                Console.Write("2- MOSTRAR TABLA....");
+                Console.Write("3- ELIMINAR..");
+                Console.Write("OP: ");
+                op = Convert.ToInt32(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                    {
+                       Console.Write("INGRESE N.CARNE.");
+                       id = Convert.ToInt32(Console.ReadLine());
+                       Console.Write("INGRESE NOMBRE..");
+                       name = Console.ReadLine();
+                       Console.Write("INGRESE APELLIDO");
+                       ape = Console.ReadLine(); 
+                       Console.Write("INGRESE TELEFONO");
+                       tele = Console.ReadLine();
+                       Console.Write("DIRECCION.......");
+                       dire = Console.ReadLine();
 
-            cLiente.Registrar(id,name,ape,tele,dire);
+                       cLiente.Registrar(id,name,ape,tele,dire);
+                       System("puase"); 
+                       System("cls");
+                       break;
+
+                    }
+                    case 2:
+                    {
+                        cLiente.Motrar();
+                        System("pause");
+                        System("cls");
+                        break;
+                    }
+                }
+            } while (op != 0);
+            
 
         }
     }
